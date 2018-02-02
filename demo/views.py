1e19c07eb738a4fd2df0eff96b18e4703bb0e8e9
Wzhixiang <13726778891@163.com>
2018-02-02 17:22:34 +0800

    学习python django

---
+++ demo/views.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.http import HttpResponse
+
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def hello(request):
+    content = {}
+    content['hello'] = "Hello, World!"
+    return render(request, 'helloworld.html', content)
